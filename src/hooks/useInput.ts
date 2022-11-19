b1769bdd6aac77b0a0cbef3014afef8526b87378
Nuri <1000nulee@naver.com>
2022-11-19 20:33:43 +0900

    change folder structure, create useInput

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import { useState, useCallback } from "react";
+
+const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const reset = useCallback(() => setValue(initialValue), [initialValue]);
+
+  return { value, onChange, reset };
+};
+
+export default useInput;
