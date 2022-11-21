325a725a28ca49b099004c0a9c42f3d4d8a2e5fd
ki226 <ki226@hallidays.io>
2022-11-21 17:43:11 +0900

    console.log 삭제

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,6 @@ export default async function getEurInfo(): Promise<EurInfoType> {
     const response = await fetch(
       "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
     );
-    console.log(response);
     if (response.ok) {
       const result = await response.json();
       return result[0];
