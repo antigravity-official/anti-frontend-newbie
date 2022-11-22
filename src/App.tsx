9fb82a067a7afe7911b2b3e3e6e772797c555c80
kwon <goodsosbva@naver.com>
2022-11-22 22:45:54 +0900

    2차 푸쉬2

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Loading from './view/Loading';
 import ExchangeRate from "./view/ExchangeRate";
 import useFetching from "./model/useFetching";
 
 export const App = () => {
   const [isReady, setReady] = useState<boolean>(false);
-  // const [eurInfo, setEurInfo] = useState<any>({});
-
-  // const getEurInfo = async () => {
-  //   const krweur = await fetch(
-  //     "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((array) => array[0]);
-
-  //   setEurInfo(krweur);
-  //   setReady(true);
-  // };
-
-  // useEffect(() => {
-  //   getEurInfo();
-  //   return () => {};
-  // }, []);
+  
   const data = useFetching(
     "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR",
     setReady
