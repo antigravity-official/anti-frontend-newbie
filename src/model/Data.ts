f0961b4e7357e3145cb35a42fdd025691d2d28d3
김지호 <crsn1111@gmail.com>
2022-11-22 16:31:07 +0900

    [feat] getData

---
+++ src/model/Data.ts
@@ -0,0 +1,9 @@
+export const getData = async () => {
+  const response = await fetch(
+    "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
+  )
+    .then((res) => res.json())
+    .then((array) => array[0]);
+  console.log(response);
+  return response;
+};
