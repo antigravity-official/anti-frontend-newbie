5b10f1b563c8152d89bcd52d04b3971483eb10ab
김지호 <crsn1111@gmail.com>
2022-11-24 14:17:32 +0900

    [feat] defaultValue 설정

--- src/hooks/useExchange.ts
+++ src/hooks/useExchange.ts
@@ -3,8 +3,9 @@ import { useState } from "react";
 import { useCalMoney } from "./useCalMoney";
 
 export const useExchange = (basePrice: number) => {
-  const [exchange, setExchange] = useState<string>("");
+  const [exchange, setExchange] = useState<string>("1");
   const regexp = /^\d*.?\d{0,2}$/;
+
   const exchangeEurToKrw = (value: string) => {
     if (!regexp.test(value)) {
       return alert("소수점 두 자리까지 가능합니다.");
