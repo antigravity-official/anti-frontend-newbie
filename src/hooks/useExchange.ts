4f67af240b963cbc8c0acda5c7fda50aa370469e
김지호 <crsn1111@gmail.com>
2022-11-24 12:39:08 +0900

    [feat] input onchange 소수 점 확인

---
+++ src/hooks/useExchange.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { useCalMoney } from "./useCalMoney";
+
+export const useExchange = (basePrice: number) => {
+  const [exchange, setExchange] = useState<string>("");
+  const regexp = /^\d*.?\d{0,2}$/;
+  const exchangeEurToKrw = (value: string) => {
+    if (!regexp.test(value)) {
+      return alert("소수점 두 자리까지 가능합니다.");
+    }
+    setExchange(value);
+  };
+
+  const Krw = useCalMoney(exchange, basePrice);
+
+  return { Krw, exchangeEurToKrw, exchange };
+};
