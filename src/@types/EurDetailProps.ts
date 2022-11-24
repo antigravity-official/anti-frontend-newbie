5339955ac6815337d3da63a574184682debdcf04
김지호 <crsn1111@gmail.com>
2022-11-24 14:16:02 +0900

    [fix] type 수정

--- src/@types/EurDetailProps.ts
+++ src/@types/EurDetailProps.ts
@@ -2,16 +2,11 @@ import { EurInfoProps } from "./EurInfoProps";
 
 export type PriceDetailProps = Pick<
   EurInfoProps,
-  | "basePrice"
-  | "openingPrice"
-  | "changePrice"
-  | "cashBuyingPrice"
-  | "cashSellingPrice"
-  | "ttSellingPrice"
-  | "ttBuyingPrice"
+  "basePrice" | "openingPrice" | "changePrice"
 >;
 
 export interface EurInfoContainerProps extends PriceDetailProps {
   moneyState: () => string;
   percent: () => string;
+  exchangeDate: () => string;
 }
