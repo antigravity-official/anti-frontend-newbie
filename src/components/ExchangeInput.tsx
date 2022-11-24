5a42a149c5cf4575a113d6e58f4806566a550c5a
김지호 <crsn1111@gmail.com>
2022-11-24 14:16:49 +0900

    [refactor] 컴포넌트 분리

--- src/components/ExchangeInput.tsx
+++ src/components/ExchangeInput.tsx
@@ -1,16 +1,25 @@
 import { useExchange } from "../hooks/useExchange";
+import Input from "./common/Input";
+import InputBox from "./common/InputBox";
 
 const ExchangeInput = ({ basePrice }: { basePrice: number }) => {
   const { exchange, exchangeEurToKrw, Krw } = useExchange(basePrice);
   return (
-    <section>
-      <input
-        type="text"
-        onChange={(e) => exchangeEurToKrw(e.currentTarget.value)}
-        value={exchange}
-      />
-      유로 ▶︎ <input disabled value={Krw} /> 원
-    </section>
+    <article className="w-full space-y-4 text-center">
+      <InputBox>
+        <Input
+          type="text"
+          onChange={(e) => exchangeEurToKrw(e.currentTarget.value)}
+          value={exchange}
+          autoFoucs={true}
+        />
+        <span className="exChangeSpan">유로</span>
+      </InputBox>
+      <InputBox>
+        <Input type="text" disabled={true} value={Krw} />
+        <span className="exChangeSpan">대한민국 원</span>
+      </InputBox>
+    </article>
   );
 };
 
