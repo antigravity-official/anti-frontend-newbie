c9081f40611c567beca0bf4d98eb4336d37704d9
Danbee Kim <kdb2360@gmail.com>
2022-11-24 12:51:02 +0900

    유로화 소수점 2자리까지 표시

--- src/components/InputExchange.tsx
+++ src/components/InputExchange.tsx
@@ -8,10 +8,12 @@ interface Euro{
   eurInfo:EuroInfo;
   }
 
+
 const InputExchange = ({eurInfo}:Euro):JSX.Element => {
-  const [money,setMoney]=useState(0)
+  const [money,setMoney]=useState<number>(0)
   const exchangeEurToKrw = (krw: number):number => krw * eurInfo.basePrice;
   const exchangedMoney=Math.floor(exchangeEurToKrw(money)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
 
   return (
     <div>
@@ -19,7 +21,10 @@ const InputExchange = ({eurInfo}:Euro):JSX.Element => {
        type="number"
        value={money||""}
        min={0}
-       onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setMoney(Number(e.target.value))}
+       onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
+        const float = parseFloat(e.target.value)
+          setMoney(Number(float.toFixed(2)))
+       }}
        /> 유로 ▶︎ 
        <input 
        disabled 
