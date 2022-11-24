1c8637ab790b68eb250b90748b89d8c35519fa15
김지호 <crsn1111@gmail.com>
2022-11-24 12:36:31 +0900

    [del] delete file

--- src/viewModel/ExchangeProvider.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "../pages/Home";
-import { useExchangeViewModel } from "./ExchangeViewModel";
-
-const ExchangeProvider = () => {
-  const ExchangeViewModel = useExchangeViewModel();
-  return <Home {...ExchangeViewModel} />;
-};
-
-export default ExchangeProvider;
