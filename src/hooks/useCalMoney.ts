1e71180a508e2ef3d8ccaa9f02884451f4d18851
김지호 <crsn1111@gmail.com>
2022-11-24 14:37:16 +0900

    [fix] 원 단위 소수점 제거

--- src/hooks/useCalMoney.ts
+++ src/hooks/useCalMoney.ts
@@ -1,5 +1,5 @@
 export const useCalMoney = (exchange: string, basePrice: number) => {
-  const Krw = (+exchange * basePrice).toFixed(2);
+  const Krw = Math.floor(+exchange * basePrice);
   const result = Krw.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   return result;
 };
