0c0a9fe4934b51b104ef043198c57cd67f212a1c
김지호 <crsn1111@gmail.com>
2022-11-24 12:37:08 +0900

    [docs] 문서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import ExchangeProvider from "./viewModel/ExchangeProvider";
 import "../src/styles/globals.css";
+import Loading from "./components/common/Loading";
+import EurInfoContainer from "./components/EurInfoContainer";
+import ExchangeInput from "./components/ExchangeInput";
+import { useFetch } from "./hooks/useFetch";
+import { exchangeViewModel } from "./viewModel/exchangeViewModel";
 
 export const App = () => {
-  return <ExchangeProvider />;
+  const { isReady, eurInfo } = useFetch();
+
+  const priceDetail = new exchangeViewModel(eurInfo);
+  return (
+    <main>
+      <article>
+        {!isReady || !eurInfo ? (
+          <Loading />
+        ) : (
+          <>
+            <EurInfoContainer priceDetail={priceDetail} />
+            <hr />
+            <ExchangeInput basePrice={eurInfo.basePrice} />
+          </>
+        )}
+      </article>
+    </main>
+  );
 };
 
 export default App;
