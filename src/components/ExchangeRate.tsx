790b3b917e97a55c9e854b3058bdd260462c6739
rnfltpgus <jpp901220@gmail.com>
2022-11-27 22:10:58 +0900

    💈 Style: 스타일 적용, 공용컴포넌트 생성

--- src/components/ExchangeRate.tsx
+++ src/components/ExchangeRate.tsx
@@ -1,4 +1,7 @@
 import { EurInfo } from '../types/EurInfo.types';
+import TitleName from './common/TitleName';
+
+import styled from '@emotion/styled';
 
 interface ExchangeRateProps {
   eurInfo: EurInfo;
@@ -9,23 +12,43 @@ const ExchangeRate = ({ eurInfo }: ExchangeRateProps) => {
     price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
   return (
-    <div>
-      <div>환율기준 (1 유로)</div>
-      <div>
-        {eurInfo.basePrice}
-        {eurInfo.basePrice - eurInfo.openingPrice > 0 && '▲'}
-        {eurInfo.basePrice - eurInfo.openingPrice < 0 && '▼'}
-        {eurInfo.changePrice}원 (
-        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
-      </div>
-      <div>
-        <div>살때 : {threeCommaFn(eurInfo.cashBuyingPrice)}원</div>
-        <div>팔때 : {threeCommaFn(eurInfo.cashSellingPrice)}원</div>
-        <div>보낼때 : {threeCommaFn(eurInfo.ttSellingPrice)}원</div>
-        <div>받을때 : {threeCommaFn(eurInfo.ttBuyingPrice)}원</div>
-      </div>
-    </div>
+    <>
+      <TitleName title='💰 환율기준 (1 유로)' />
+      <ExchangeRateContainer>
+        <ExchangeRateTop>
+          <TitleName title='실시간 환율' />
+          <div>
+            {eurInfo.basePrice}
+            {eurInfo.basePrice - eurInfo.openingPrice > 0 && '▲'}
+            {eurInfo.basePrice - eurInfo.openingPrice < 0 && '▼'}
+            {eurInfo.changePrice}원 (
+            {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+          </div>
+        </ExchangeRateTop>
+        <ExchangeRateBottom>
+          <TitleName title='시세 체크' />
+          <div>살때 : {threeCommaFn(eurInfo.cashBuyingPrice)}원</div>
+          <div>팔때 : {threeCommaFn(eurInfo.cashSellingPrice)}원</div>
+          <div>보낼때 : {threeCommaFn(eurInfo.ttSellingPrice)}원</div>
+          <div>받을때 : {threeCommaFn(eurInfo.ttBuyingPrice)}원</div>
+        </ExchangeRateBottom>
+      </ExchangeRateContainer>
+    </>
   );
 };
 
 export default ExchangeRate;
+
+const ExchangeRateContainer = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.3rem;
+  padding: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const ExchangeRateTop = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const ExchangeRateBottom = styled.div``;
---
+++ src/components/common/TitleName.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+interface TitleNameProps {
+  title: string;
+}
+
+const TitleName = ({ title }: TitleNameProps) => {
+  return <TitleNameContainer>{title}</TitleNameContainer>;
+};
+
+export default TitleName;
+
+const TitleNameContainer = styled.span`
+  font-size: 1.2rem;
+  font-weight: bold;
+  border: 1rem;
+  margin-right: 1rem;
+`;
