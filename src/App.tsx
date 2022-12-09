b40da1a30fe32c9e3a97a16ab5c453db29ee0c57
rivera00255 <rivera00255@gmail.com>
2022-12-09 10:41:52 +0900

    Feat: 환율 계산 & 원화 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+type Price = {
+  basePrice: number;
+  openingPrice: number;
+  changePrice: number;
+  cashBuyingPrice: number;
+  cashSellingPrice: number;
+  ttSellingPrice: number;
+  ttBuyingPrice: number;
+  [key: string]: any;
+};
+
 export const App = () => {
   const [isReady, setReady] = useState(false);
-  const [eurInfo, setEurInfo] = useState<any>({});
+  const [eurInfo, setEurInfo] = useState<{ [key: string]: any }>({});
+  const [krwPrice, setKrwPrice] = useState(0);
 
   const getEurInfo = async () => {
-    const krweur = await fetch(
+    const krweur: Price = await fetch(
       "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
     )
       .then((response) => response.json())
@@ -14,8 +26,11 @@ export const App = () => {
     setEurInfo(krweur);
     setReady(true);
   };
+  console.log(eurInfo);
 
-  const exchangeEurToKrw = (krw: any) => krw * eurInfo.basePrice;
+  const exchangeEurToKrw = (krw: number) => {
+    setKrwPrice(krw * eurInfo.basePrice);
+  };
 
   useEffect(() => {
     getEurInfo();
@@ -27,20 +42,34 @@ export const App = () => {
     <div className="App">
       <div>환율기준 (1 유로)</div>
       <div>
-        {eurInfo.basePrice}
+        {eurInfo.basePrice.toLocaleString().split(".")[0]}
         {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
         {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
         {eurInfo.changePrice}원 (
         {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
       </div>
       <div>
-        <div>살때 : {eurInfo.cashBuyingPrice}</div>
-        <div>팔때 : {eurInfo.cashSellingPrice}</div>
-        <div>보낼때 : {eurInfo.ttSellingPrice}</div>
-        <div>받을때 : {eurInfo.ttBuyingPrice}</div>
+        <div>
+          살때 : {eurInfo.cashBuyingPrice.toLocaleString().split(".")[0]}
+        </div>
+        <div>
+          팔때 : {eurInfo.cashSellingPrice.toLocaleString().split(".")[0]}
+        </div>
+        <div>
+          보낼때 : {eurInfo.ttSellingPrice.toLocaleString().split(".")[0]}
+        </div>
+        <div>
+          받을때 : {eurInfo.ttBuyingPrice.toLocaleString().split(".")[0]}
+        </div>
       </div>
       <hr />
-      <input /> 유로 ▶︎ <input disabled /> 원
+      <input
+        defaultValue={0}
+        onChange={(e) => exchangeEurToKrw(Number(e.target.value))}
+      />{" "}
+      유로 ▶︎{" "}
+      <input value={krwPrice.toLocaleString().split(".")[0] || ""} disabled />{" "}
+      원
     </div>
   );
 };
