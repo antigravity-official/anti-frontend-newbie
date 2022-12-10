50d12c1a6ce5fc814fc5a47bd38f3485fafbaa2a
stardust6653 <stardust6653@gmail.com>
2022-12-10 20:31:00 +0900

    Style : styled-component props 사용 및 타입지정 & 가격 상황에 따라 폰트 색상 및 아이콘 변경
    - styled-components 공통되게 묶을 수 있는 컴포넌트는 하나로 합치고 props를 활용
    - 현재 가격과 오프닝 가격의 상관관계에 따라 상승, 하락 구분
    - 각 타입 지정

--- src/components/CurrentBasePrice.tsx
+++ src/components/CurrentBasePrice.tsx
@@ -1,38 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 import { PriceType } from "../types/types";
+import PriceComponent from "./public/PriceComponent";
 
-const BasePriceComponent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #000;
-  border-radius: 1rem;
-  padding: 1.5rem;
+interface ElementStyle {
+  fontSize: string;
+  fontWeight: number;
+  marginBottom: string;
+}
+
+const ElementInPriceComponent = styled.div<ElementStyle>`
+  font-size: ${props => props.fontSize};
+  font-weight: ${props => props.fontWeight};
+  margin-bottom: ${props => props.marginBottom};
 `;
 
-const BasePriceEl = styled.div``;
-
-const UpAndDownEl = styled.div``;
-
-const ChangePriceEl = styled.div``;
+const ChangePriceEl = styled.div`
+  font-size: 1.1rem;
+  color: ${props => props.color};
+`;
 
 const CurrentBasePrice = ({ basePrice, openingPrice, changePrice }: PriceType): JSX.Element => {
   const changePercent = ((changePrice / basePrice) * 100).toFixed(2);
 
   return (
-    <BasePriceComponent>
-      <BasePriceEl>{basePrice}</BasePriceEl>
-      <UpAndDownEl>
-        {basePrice > openingPrice ? "📈" : "📉"}
+    <PriceComponent>
+      <ElementInPriceComponent fontSize={"1.3rem"} fontWeight={700} marginBottom={"0.2rem"}>
+        {basePrice}원
+      </ElementInPriceComponent>
+      <ElementInPriceComponent fontSize={"1.2rem"} fontWeight={500} marginBottom={"0.7rem"}>
+        {basePrice > openingPrice ? "📈 " : "📉 "}
         {changePrice}원
-      </UpAndDownEl>
-      <ChangePriceEl>
+      </ElementInPriceComponent>
+      <ChangePriceEl color={`${basePrice > openingPrice ? "red" : "blue"}`}>
         {basePrice > openingPrice ? "+" : "-"}
         {changePercent}%
       </ChangePriceEl>
-    </BasePriceComponent>
+    </PriceComponent>
   );
 };
 
