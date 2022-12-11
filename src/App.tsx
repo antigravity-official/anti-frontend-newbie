13ed2fcb176518951f336d8d745762c3d9336a3b
stardust6653 <stardust6653@gmail.com>
2022-12-11 15:35:08 +0900

    chore : 사용하지 않는 import 파일 정리
    - styled-component 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { API_KEY } from "./api/API_KEY";
 import { useGetData } from "./Hook/useGetData";
 import Title from "./components/Title";
