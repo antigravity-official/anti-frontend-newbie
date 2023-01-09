2096d2bef395d8c5e99707a354d1dc0d69f7655a
schan <schan@openerd.com>
2023-01-09 17:53:59 +0900

    chore : webpack 커서마이징 및 yarn start 명령어 변경

--- config-overrides.js
+++ config-overrides.js
@@ -5,13 +5,12 @@ const { override, addWebpackAlias } = require('customize-cra');
 module.exports = override(
   addWebpackAlias({
     '@api': path.resolve(__dirname, 'src/api'),
-    '@assets': path.resolve(__dirname, 'src/assets'),
     '@constants': path.resolve(__dirname, 'src/constants'),
     '@components': path.resolve(__dirname, 'src/components'),
     '@hooks': path.resolve(__dirname, 'src/hooks'),
     '@store': path.resolve(__dirname, 'src/store'),
     '@styles': path.resolve(__dirname, 'src/styles'),
-    '@type': path.resolve(__dirname, 'src/type'),
+    '@types': path.resolve(__dirname, 'src/types'),
     '@utils': path.resolve(__dirname, 'src/utils'),
   }),
 );
