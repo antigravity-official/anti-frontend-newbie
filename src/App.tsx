fd63722c38fe04c3b4cf53bc0c46df68782f14e7
Bong <2021bongwonny@gmail.com>
2023-01-09 17:09:41 +0900

    Add : 수행 항목 기능 구현 1. Type 지정 3. 유로화 입력시 환율 반영하여 원화로 표시(exchangeEurToKrw 함수 사용) 5. 금액 세 자리마다 콤마(,) 표시 6. 유로화 소수점 2자리까지 입력, 원화는 소수점 표시하지 않음

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from 'react';
+
+interface EuroInfo {
+  basePrice: number;
+  openingPrice: number;
+  changePrice: number;
+  cashBuyingPrice: number;
+  cashSellingPrice: number;
+  ttSellingPrice: number;
+  ttBuyingPrice: number;
+}
 
 export const App = () => {
   const [isReady, setReady] = useState(false);
-  const [eurInfo, setEurInfo] = useState<any>({});
+  const [eurInfo, setEurInfo] = useState<EuroInfo | undefined>();
+  const [inputEuro, setInputEuro] = useState('');
 
   const getEurInfo = async () => {
     const krweur = await fetch(
-      "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
+      'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR'
     )
       .then((response) => response.json())
       .then((array) => array[0]);
@@ -15,7 +26,29 @@ export const App = () => {
     setReady(true);
   };
 
-  const exchangeEurToKrw = (krw: any) => krw * eurInfo.basePrice;
+  const exchangeEurToKrw = (krw: string) => {
+    if (!!!inputEuro) return '';
+    if (!!eurInfo) {
+      return Math.floor(Number(krw) * eurInfo.basePrice);
+    }
+  };
+
+  const addComma = (money: number | string | undefined) => {
+    if (money === '' || typeof money === 'undefined') return '';
+    return money?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const transferInt = (float: number) => {
+    return Math.floor(float);
+  };
+
+  const handleWriteEuro = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    if (value.includes('.')) {
+      value = value.split('.')[0] + '.' + value.split('.')[1].slice(0, 2);
+    }
+    setInputEuro(value);
+  };
 
   useEffect(() => {
     getEurInfo();
@@ -24,23 +57,28 @@ export const App = () => {
 
   if (!isReady) return null;
   return (
-    <div className="App">
+    <div className='App'>
       <div>환율기준 (1 유로)</div>
-      <div>
-        {eurInfo.basePrice}
-        {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
-        {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
-        {eurInfo.changePrice}원 (
-        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
-      </div>
-      <div>
-        <div>살때 : {eurInfo.cashBuyingPrice}</div>
-        <div>팔때 : {eurInfo.cashSellingPrice}</div>
-        <div>보낼때 : {eurInfo.ttSellingPrice}</div>
-        <div>받을때 : {eurInfo.ttBuyingPrice}</div>
-      </div>
+      {!!eurInfo && (
+        <>
+          <div>
+            {addComma(transferInt(eurInfo.basePrice))}
+            {eurInfo.basePrice - eurInfo.openingPrice > 0 && '▲'}
+            {eurInfo.basePrice - eurInfo.openingPrice < 0 && '▼'}
+            {addComma(eurInfo.changePrice)}원 (
+            {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+          </div>
+          <div>
+            <div>살때 : {addComma(transferInt(eurInfo.cashBuyingPrice))}</div>
+            <div>팔때 : {addComma(transferInt(eurInfo.cashSellingPrice))}</div>
+            <div>보낼때 : {addComma(transferInt(eurInfo.ttSellingPrice))}</div>
+            <div>받을때 : {addComma(transferInt(eurInfo.ttBuyingPrice))}</div>
+          </div>
+        </>
+      )}
       <hr />
-      <input /> 유로 ▶︎ <input disabled /> 원
+      <input value={inputEuro} onChange={handleWriteEuro} /> 유로 ▶︎&nbsp;
+      <input disabled value={addComma(exchangeEurToKrw(inputEuro))} /> 원
     </div>
   );
 };
