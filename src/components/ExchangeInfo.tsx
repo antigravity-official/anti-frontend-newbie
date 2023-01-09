46c7b3de1583091cd6419039ce39d3481039d03d
WookyungYun <gyong1540@gmail.com>
2023-01-10 00:19:26 +0900

    로딩 스켈레톤 추가

--- src/components/ExchangeInfo.tsx
+++ src/components/ExchangeInfo.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { ExchangeInfoType } from "../type/exchange";
 import { getInfo } from "../api/exchangeInfo";
 import ExchangeCalc from "./ExchangeCalc";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function ExchangeInfo() {
   const [isReady, setReady] = useState(false);
@@ -19,26 +21,30 @@ export default function ExchangeInfo() {
     getEurInfo();
     return () => {};
   }, []);
-
-  if (!isReady) return null;
   return (
     <div className="App">
       <div>환율기준 (1 유로)</div>
-      <div>
-        {eurInfo.basePrice}
-        {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
-        {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
-        {eurInfo.changePrice}원 (
-        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
-      </div>
+      {isReady === false ? (
+        <Skeleton count={6} width="170px" />
+      ) : (
+        <>
+          <div>
+            {eurInfo.basePrice}
+            {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
+            {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
+            {eurInfo.changePrice}원 (
+            {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+          </div>
 
-      <div>
-        <div>살때 : {eurInfo.cashBuyingPrice}</div>
-        <div>팔때 : {eurInfo.cashSellingPrice}</div>
-        <div>보낼때 : {eurInfo.ttSellingPrice}</div>
-        <div>받을때 : {eurInfo.ttBuyingPrice}</div>
-      </div>
-      <ExchangeCalc eurInfo={eurInfo} />
+          <div>
+            <div>살때 : {eurInfo.cashBuyingPrice}</div>
+            <div>팔때 : {eurInfo.cashSellingPrice}</div>
+            <div>보낼때 : {eurInfo.ttSellingPrice}</div>
+            <div>받을때 : {eurInfo.ttBuyingPrice}</div>
+          </div>
+          <ExchangeCalc eurInfo={eurInfo} />
+        </>
+      )}
     </div>
   );
 }
