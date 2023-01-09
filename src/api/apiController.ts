42fe75353c9e84bfd962066ed2bd926cb7bffa29
songgaram <pager1436@gmail.com>
2023-01-09 22:15:12 +0900

    chore : axios install
    api 인스턴스 추상화하여 api controller 추가

---
+++ src/api/apiController.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const Axios = axios.create({
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+  },
+});
+
+Axios.interceptors.request.use(
+  (config) => config,
+  (error) => {
+    // 요청에 대한 오류 발생 시, 오류 내용을 출력하고 요청을 거절함
+    console.log("🚀 ~ request error : ", error);
+    alert("🚀 ~ response error ");
+    Promise.reject(error);
+  }
+);
+
+// 오류 처리를 위한 별도 errorController
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log("🚀 ~ response error : ", error);
+    alert("🚀 ~ response error ");
+    Promise.reject(error);
+  }
+);
+
+export default Axios;
