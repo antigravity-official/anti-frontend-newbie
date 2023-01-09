5aea4ac44e80b16a2300ef9e52e0d4e6210cc5ff
Sara-Jo <sr930927@gmail.com>
2023-01-09 20:15:53 +0900

    modified types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import EurInfo from "./interfaces/EurInfo";
 
 export const App = () => {
-  const [isReady, setReady] = useState(false);
-  const [eurInfo, setEurInfo] = useState<any>({});
+  const [isReady, setReady] = useState<boolean>(false);
+  const [eurInfo, setEurInfo] = useState<EurInfo | null>(null);
   const [exchangedKrw, setExchangedKrw] = useState<number>();
 
   const getEurInfo = async () => {
@@ -16,10 +17,12 @@ export const App = () => {
     setReady(true);
   };
 
-  const exchangeEurToKrw = (eur: number) => eur * eurInfo.basePrice;
+  const exchangeEurToKrw = (eur: number) =>
+    eurInfo ? eur * eurInfo.basePrice : null;
 
-  const handleEurInput = (e: any) => {
-    setExchangedKrw(Math.floor(exchangeEurToKrw(e.target.value)));
+  const handleEurInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const krw = exchangeEurToKrw(Number(e.target.value));
+    krw && setExchangedKrw(Math.floor(krw));
   };
 
   useEffect(() => {
@@ -31,21 +34,23 @@ export const App = () => {
   return (
     <div className="App">
       <div>환율기준 (1 유로)</div>
+      {eurInfo && (
+        <div>
+          {eurInfo.basePrice}
+          {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
+          {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
+          {eurInfo.changePrice}원 (
+          {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+        </div>
+      )}
       <div>
-        {eurInfo.basePrice}
-        {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
-        {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
-        {eurInfo.changePrice}원 (
-        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
-      </div>
-      <div>
-        <div>살때 : {eurInfo.cashBuyingPrice}</div>
-        <div>팔때 : {eurInfo.cashSellingPrice}</div>
-        <div>보낼때 : {eurInfo.ttSellingPrice}</div>
-        <div>받을때 : {eurInfo.ttBuyingPrice}</div>
+        <div>살때 : {eurInfo?.cashBuyingPrice}</div>
+        <div>팔때 : {eurInfo?.cashSellingPrice}</div>
+        <div>보낼때 : {eurInfo?.ttSellingPrice}</div>
+        <div>받을때 : {eurInfo?.ttBuyingPrice}</div>
       </div>
       <hr />
-      <input onChange={handleEurInput} /> 유로 ▶︎{" "}
+      <input type="number" onChange={handleEurInput} /> 유로 ▶︎{" "}
       <input value={exchangedKrw} disabled /> 원
     </div>
   );
