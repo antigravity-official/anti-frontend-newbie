7ec0d3b1e575c2ab35a13ae3fc347a956e79ea55
songgaram <pager1436@gmail.com>
2023-01-10 22:54:27 +0900

    refact : views의 index 삭제

--- src/routes/RoutesViews.tsx
+++ src/routes/RoutesViews.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ExchangeRate from "../views";
+import Currency from "../views/currency/Currency";
 
 const RoutesPage = () => {
   return (
     <Routes>
-      <Route path="/" element={<ExchangeRate />} />
+      <Route path="/" element={<Currency />} />
     </Routes>
   );
 };
--- src/views/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Currency from "./currency/Currency";
-
-const ExchangeRate = () => {
-  return <Currency />;
-};
-
-export default ExchangeRate;
