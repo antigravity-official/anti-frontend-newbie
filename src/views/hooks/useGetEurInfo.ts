fd9ce05fa82d03d9427efcd46d60b9fc3cefdcdc
songgaram <pager1436@gmail.com>
2023-01-10 13:17:16 +0900

    feat : type 따로 분리

--- src/views/hooks/useGetEurInfo.ts
+++ src/views/hooks/useGetEurInfo.ts
@@ -1,16 +1,7 @@
 import { useQuery } from "react-query";
 import { getEurInfo } from "../api/api";
 import { AxiosError } from "axios";
-
-interface FetchData {
-  basePrice: number;
-  openingPrice: number;
-  changePrice: number;
-  cashBuyingPrice: number;
-  cashSellingPrice: number;
-  ttSellingPrice: number;
-  ttBuyingPrice: number;
-}
+import { FetchData } from "../types";
 
 const useGetEurInfo = () => {
   return useQuery<FetchData, AxiosError>(["eur"], () => getEurInfo(), {
