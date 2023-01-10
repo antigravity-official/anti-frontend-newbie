35c1f877b3c8dd6a7a11c9f62402772a9068ff82
schan <schan@openerd.com>
2023-01-11 02:09:24 +0900

    refactor : 불필요한 경로 삭제 및 경로 변경

--- config-overrides.js
+++ config-overrides.js
@@ -5,12 +5,13 @@ const { override, addWebpackAlias } = require('customize-cra');
 module.exports = override(
   addWebpackAlias({
     '@api': path.resolve(__dirname, 'src/api'),
-    '@constants': path.resolve(__dirname, 'src/constants'),
     '@components': path.resolve(__dirname, 'src/components'),
+    '@constants': path.resolve(__dirname, 'src/constants'),
     '@hooks': path.resolve(__dirname, 'src/hooks'),
-    '@store': path.resolve(__dirname, 'src/store'),
+    '@model': path.resolve(__dirname, 'src/model'),
+    '@pages': path.resolve(__dirname, 'src/pages'),
     '@styles': path.resolve(__dirname, 'src/styles'),
-    '@types': path.resolve(__dirname, 'src/types'),
+    '@type': path.resolve(__dirname, 'src/type'),
     '@utils': path.resolve(__dirname, 'src/utils'),
   }),
 );
