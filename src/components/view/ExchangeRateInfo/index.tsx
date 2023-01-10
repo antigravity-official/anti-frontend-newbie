412138cdc461388360ea0b57ed55fb5234dc4b18
schan <schan@openerd.com>
2023-01-11 02:14:54 +0900

    style : text '원' 추가

--- src/components/view/ExchangeRateInfo/index.tsx
+++ src/components/view/ExchangeRateInfo/index.tsx
@@ -15,10 +15,10 @@ const ExchangeRateInfo = ({
 }: ExchangeRateInfoPropsType) => {
   return (
     <S.Wrapper>
-      <S.Text>살때 : {cashBuyingPrice}</S.Text>
-      <S.Text>팔때 : {cashSellingPrice}</S.Text>
-      <S.Text>보낼때 : {ttSellingPrice}</S.Text>
-      <S.Text>받을때 : {ttBuyingPrice}</S.Text>
+      <S.Text>살때 : {cashBuyingPrice}원</S.Text>
+      <S.Text>팔때 : {cashSellingPrice}원</S.Text>
+      <S.Text>보낼때 : {ttSellingPrice}원</S.Text>
+      <S.Text>받을때 : {ttBuyingPrice}원</S.Text>
     </S.Wrapper>
   );
 };
