19334344460ba486a4228fd67668fdbb28bd872c
hyebin829 <smilerain779@gmail.com>
2023-01-11 14:10:03 +0900

    Add exchange rate data type

---
+++ src/types/data.d.ts
@@ -0,0 +1,10 @@
+export interface IexchangeRate {
+  basePrice: number | string
+  changePrice: number | string
+  cashBuyingPrice: number | string
+  cashSellingPrice: number | string
+  ttSellingPrice: number | string
+  ttBuyingPrice: number | string
+  changeRate: number | string
+  rate: number | string
+}
