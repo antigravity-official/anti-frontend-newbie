8d44c131ffdf0285f2a82108693f51f8f89a97ee
hyebin829 <smilerain779@gmail.com>
2023-01-11 14:57:58 +0900

    Refactor : make useformatting hook

--- src/ExchangeRateViewModel.tsx
+++ src/ExchangeRateViewModel.tsx
@@ -1,34 +1,24 @@
-import { ChangeEvent, useContext, useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, useContext, useEffect, useState } from 'react'
 
 import { ExchangeContext } from './context/exchangerate-context'
-import { IexchangeRate } from './types/data.d'
+
+import useFormatting from './hooks/useFormatting'
 
 import ExchangeRateBoard from './components/ExchangeRateList'
 import ExchangeInput from './components/ExchangeInput'
 
 const ExchangeRateViewModel = () => {
+  const [eur, setEur] = useState<string>()
+  const [krw, setKrw] = useState<string>()
+
   const exchangeData = useContext(ExchangeContext).data
   const loading = useContext(ExchangeContext).isFetching
 
-  const [eur, setEur] = useState<string>()
-  const [krw, setKrw] = useState<string>()
-  const [data, setData] = useState<IexchangeRate>()
-  const options = useMemo(() => ({ maximumFractionDigits: 0 }), [])
+  const data = useFormatting(exchangeData)
 
-  useEffect(() => {
-    setData({
-      basePrice: exchangeData?.basePrice.toLocaleString('ko-KR', {
-        maximumFractionDigits: 0,
-      }),
-      changePrice: exchangeData?.changePrice.toLocaleString('ko-KR', options),
-      cashBuyingPrice: exchangeData?.cashBuyingPrice.toLocaleString('ko-KR', options),
-      cashSellingPrice: exchangeData?.cashSellingPrice.toLocaleString('ko-KR', options),
-      ttSellingPrice: exchangeData?.ttSellingPrice.toLocaleString('ko-KR', options),
-      ttBuyingPrice: exchangeData?.ttBuyingPrice.toLocaleString('ko-KR', options),
-      changeRate: Number(exchangeData?.basePrice) - Number(exchangeData?.openingPrice),
-      rate: (Number(exchangeData?.changePrice) / Number(exchangeData?.basePrice)) * 100,
-    })
-  }, [exchangeData, options])
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setEur(e.target.value)
+  }
 
   useEffect(() => {
     if (eur && exchangeData.basePrice) {
@@ -39,10 +29,6 @@ const ExchangeRateViewModel = () => {
       )
     }
   }, [exchangeData?.basePrice, eur])
-
-  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setEur(e.target.value)
-  }
 
   if (loading) return <div>로딩..</div>
 
