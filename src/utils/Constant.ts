b6fba42166838b9f3d1e0bb09f4aaf5a95e82d4f
Hungeun2 <bepoohun@gmail.com>
2023-01-11 20:40:06 +0900

    feat: 상수 추가
    exchange 문구 관련

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -1,3 +1,17 @@
 export const ENDPOINT = Object.freeze(
   'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR'
 );
+export const EXCHANGE_MSG = Object.freeze({
+  STANDARD: '환율기준 (1 유로)',
+  KRW: '원',
+  EUR: '유로',
+  RISE: 'RISE',
+  FALL: 'FALL',
+  RISE_SIGN: '▲',
+  FALL_SIGN: '▼',
+  EQUAL: '▶︎',
+  CASH_BUY: '살때 : ',
+  CASH_SELL: '팔때 : ',
+  TT_BUY: '받을때 : ',
+  TT_SELL: '보낼때 : ',
+});
