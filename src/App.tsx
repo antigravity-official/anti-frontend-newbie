a98ecd6c001e291e8d9c04fbb38af9786bde0861
hana1203 <hanaindec@gmail.com>
2023-01-12 08:56:04 +0900

    viewmodel에 view를 바인딩, 모든 종속성 연결하는 provider작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { NumericFormat } from "react-number-format";
+import { Provider } from "./Provider";
 
-import { EuroInfo } from "./types/type";
-import { ExchangeRateInfoView } from "./components/ExchangeRateInfoView";
-import { ExchangeRateInfoViewModel } from "./viewmodel/ExchangeRateInfoViewModel";
-import { ExchangeRateInfoModel } from "./model/ExchangeRateInfoModel";
-
-export const App = () => {
-  const euroInfo = ExchangeRateInfoViewModel(new ExchangeRateInfoModel());
-  // useEffect(() => {
-  //   getEurInfo();
-  //   return () => {};
-  // }, []);
-
-  // if (!isReady) return null;
+const App = () => {
   return (
     <div className="App">
-      <ExchangeRateInfoView euroInfo={euroInfo}></ExchangeRateInfoView>
+      <Provider></Provider>
     </div>
   );
 };
