20f15c8a718f2bedc13e9b69470ab029e5bf41e2
hyebin829 <smilerain779@gmail.com>
2023-01-11 10:46:01 +0900

    Add : eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 export const App = () => {
-  const [isReady, setReady] = useState(false);
-  const [eurInfo, setEurInfo] = useState<any>({});
+  const [isReady, setReady] = useState(false)
+  const [eurInfo, setEurInfo] = useState<any>({})
 
   const getEurInfo = async () => {
-    const krweur = await fetch(
-      "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
-    )
+    const krweur = await fetch('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR')
       .then((response) => response.json())
-      .then((array) => array[0]);
+      .then((array) => array[0])
 
-    setEurInfo(krweur);
-    setReady(true);
-  };
+    setEurInfo(krweur)
+    setReady(true)
+  }
 
-  const exchangeEurToKrw = (krw: any) => krw * eurInfo.basePrice;
+  console.log(eurInfo)
+
+  const exchangeEurToKrw = (krw: any) => krw * eurInfo.basePrice
 
   useEffect(() => {
-    getEurInfo();
-    return () => {};
-  }, []);
+    getEurInfo()
+    return () => {}
+  }, [])
 
-  if (!isReady) return null;
+  if (!isReady) return null
   return (
-    <div className="App">
+    <div className='App'>
       <div>환율기준 (1 유로)</div>
       <div>
         {eurInfo.basePrice}
-        {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
-        {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
-        {eurInfo.changePrice}원 (
-        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+        {eurInfo.basePrice - eurInfo.openingPrice > 0 && '▲'}
+        {eurInfo.basePrice - eurInfo.openingPrice < 0 && '▼'}
+        {eurInfo.changePrice}원 ({(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
       </div>
       <div>
         <div>살때 : {eurInfo.cashBuyingPrice}</div>
@@ -42,7 +41,7 @@ export const App = () => {
       <hr />
       <input /> 유로 ▶︎ <input disabled /> 원
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
