258e1cafeff0c6ec54d857fa7205a198c2b4e804
rovxxmjxx <miju3545@gmail.com>
2023-01-16 21:22:45 +0900

    refactor: reset input value

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,17 +1,18 @@
 import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import { useState } from 'react';
 
-type ReturnType<T> = [T, (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => T, Dispatch<SetStateAction<T>>];
+type ReturnType<T> = [T, (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => T, () => void];
 export default function useInput<T>(initialValue: T): ReturnType<T> {
   const [value, setValue] = useState(initialValue);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const value = e.target.value as unknown as T;
-
     setValue(value);
 
     return value;
   };
 
-  return [value, handleChange, setValue];
+  const handleReset = () => setValue(initialValue);
+
+  return [value, handleChange, handleReset];
 }
