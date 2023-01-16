6211d3f305cfa16451fb8974e85224c3df3fe3bf
homile <ssanho2@naver.com>
2023-01-16 23:43:56 +0900

    feat: eurInfo Type 선언

---
+++ src/types/EurInfoType.ts
@@ -0,0 +1,9 @@
+export interface EruInfoType {
+  basePrice: number;
+  openingPrice: number;
+  changePrice: number;
+  cashBuyingPrice: number;
+  cashSellingPrice: number;
+  ttSellingPrice: number;
+  ttBuyingPrice: number;
+}
