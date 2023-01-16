b7c7c724cf937ff07bff879db1c57566e423099a
junhyungkwon <wnsgud8845@gmail.com>
2023-01-16 17:10:40 +0900

    add:  File Components Configure

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export const App = () => {
     const krweur = await fetch(
       "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
     )
+    
       .then((response) => response.json())
       .then((array) => array[0]);
 
---
+++ src/interface/Type.tsx
@@ -0,0 +1,10 @@
+
+export interface EuroInfo {
+    basePrice: number;
+    openingPrice: number;
+    changePrice: number;
+    cashBuyingPrice: number;
+    cashSellingPrice: number;
+    ttSellingPrice: number;
+    ttBuyingPrice: number;
+  }
