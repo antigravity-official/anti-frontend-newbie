73eda5d4b80847013040feefed4e14774967e4df
froggy1014 <clcl6084@gmail.com>
2023-01-17 08:05:25 +0900

    feat: Create Loading Components and export at once

---
+++ src/components/UI/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <h1>Loading</h1>
+  )
+}
+
+export default Loading
---
+++ src/components/UI/index.ts
@@ -0,0 +1,11 @@
+import DashBoard from './Dashboard';
+import Input from './Input';
+import Output from './Output';
+import Loading from './Loading';
+
+export {
+  DashBoard,
+  Input,
+  Output,
+  Loading
+};
