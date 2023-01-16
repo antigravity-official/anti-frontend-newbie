3ba97b97e3f73a4551f4cf1922491a3034bd6c36
KyeongS <122796466+ksoo0503@users.noreply.github.com>
2023-01-16 22:53:07 +0900

    fix: 폴더 구조 수정

---
+++ src/components/exchange/ExchangeView.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Props {}
+
+const ExchangeView: React.FC<Props> = () => {
+  return (
+    <>
+      <div>환율기준 (1 유로)</div>
+      <div>
+        {eurInfo.basePrice}
+        {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
+        {eurInfo.basePrice - eurInfo.openingPrice < 0 && "▼"}
+        {eurInfo.changePrice}원 (
+        {(eurInfo.changePrice / eurInfo.basePrice) * 100}%)
+      </div>
+      <div>
+        <div>살때 : {eurInfo.cashBuyingPrice}</div>
+        <div>팔때 : {eurInfo.cashSellingPrice}</div>
+        <div>보낼때 : {eurInfo.ttSellingPrice}</div>
+        <div>받을때 : {eurInfo.ttBuyingPrice}</div>
+      </div>
+      <hr />
+      <input /> 유로 ▶︎ <input disabled /> 원
+    </>
+  );
+};
+export default ExchangeView;
