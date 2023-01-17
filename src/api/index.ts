160b561bc90e7442061b25ef13f158a309110f32
Kim Chansoo <cskim9410@gmail.com>
2023-01-17 22:08:55 +0900

    Refactor: api 요청url 템플릿 리터럴 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { KrwEur } from "../types/krweur";
 export const getEurInfo = async () => {
   try {
     const res = await fetch(
-      process.env.REACT_APP_API_URL! + "/v1/forex/recent?codes=FRX.KRWEUR"
+      `${process.env.REACT_APP_API_URL}/v1/forex/recent?codes=FRX.KRWEUR`
     );
     const data = await res.json();
     const krweur: KrwEur = await data[0];
