181b71e96d8bc5fe5f0f4e94e3013fa9c1b18863
junhyungkwon <wnsgud8845@gmail.com>
2023-01-17 23:37:01 +0900

    Loading Modify

--- src/views/IndicatorView.tsx
+++ src/views/IndicatorView.tsx
@@ -1,11 +1,22 @@
 // 환율 정보 로딩중임을 사용자가 알 수 있도록 UI를 추가
 // css로 꾸미기
 import React from 'react';
-import BeatLoader from "react-spinners/BeatLoader";
 
-export const IndicatorView = () => {
-  return <div>로딩중...
-  </div>;
-};
+class IndicatorView extends React.Component{
+  state = {
+    Loading: true
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({Loading: false});
+    }, 2000);
+  }
+  render() {
+    return <div>{this.state.Loading ? "로딩중입니다..." : null}
+   
+    </div>;
+  }
+}
+
 
 export default IndicatorView;
