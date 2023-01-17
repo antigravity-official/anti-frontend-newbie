780ea46339a2e60d71904b2961bbbdd0767fd827
박지윤 <jyun1937@naver.com>
2023-01-17 10:45:04 +0900

    Design: globalstyle, theme 세팅

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background: {
+      basic: string
+      point: string
+    }
+    text: {
+      basic: string
+    }
+
+    device: {
+      mobile: string
+      tablet: string
+      laptop: string
+    }
+  }
+}
