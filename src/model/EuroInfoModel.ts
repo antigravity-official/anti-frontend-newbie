185b3768b769c1763fadb489025bffcec20e9faa
Exist95 <jyeol0210@naver.com>
2023-01-17 15:39:44 +0900

    [REFECTOR] 모델 분리

---
+++ src/model/EuroInfoModel.ts
@@ -0,0 +1,13 @@
+import { EuroInfo } from "../types/types";
+
+export const EuroInfoModel = {
+  async getEuroInfo(): Promise<EuroInfo> {
+    const getEurInfo = await fetch(
+      "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
+    )
+      .then((response) => response.json())
+      .then((array) => array[0]);
+
+    return getEurInfo;
+  },
+};
