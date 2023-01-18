52ada233626cd3db8f2987486c15662e954420c9
doyu <ydh0905@nate.com>
2023-01-18 10:51:51 +0900

    Test EuroInfoView

---
+++ src/component/EuroInfo/test/EuroInfoView.test.tsx
@@ -0,0 +1,73 @@
+import renderer from 'react-test-renderer';
+import EuroInfoView from '../EuroInfoView';
+import useEuroInfoViewModel from '../EuroInfoViewModel';
+import { createWrapper } from '../../../utils/tests/utils';
+
+const mockedUseEuroInfoViewModel = useEuroInfoViewModel as jest.Mock;
+jest.mock('../EuroInfoViewModel');
+
+describe('EuroInfoView', () => {
+  let fakeApi: { euro: jest.Mock };
+
+  beforeEach(() => {
+    fakeApi = {
+      euro: jest.fn(),
+    };
+  });
+
+  afterEach(() => fakeApi.euro.mockReset());
+
+  it('renders correctly while is loading', () => {
+    mockedUseEuroInfoViewModel.mockReturnValue({
+      euro: '',
+      krw: '0',
+      eurInfo: null,
+      isLoading: true,
+      isFetching: true,
+      onChange: jest.fn(),
+    });
+    const tree = renderer
+      .create(createWrapper(<EuroInfoView />, fakeApi))
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly while is fetching', () => {
+    mockedUseEuroInfoViewModel.mockReturnValue({
+      euro: '',
+      krw: '0',
+      eurInfo: null,
+      isLoading: false,
+      isFetching: true,
+      onChange: jest.fn(),
+    });
+    const tree = renderer
+      .create(createWrapper(<EuroInfoView />, fakeApi))
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly', async () => {
+    mockedUseEuroInfoViewModel.mockReturnValue({
+      euro: '1',
+      krw: '1300',
+      eurInfo: {
+        basePrice: 1339.98,
+        cashBuyingPrice: 1366.64,
+        cashSellingPrice: 1313.32,
+        changePrice: 3.86,
+        openingPrice: 1336.18,
+        ttBuyingPrice: 1326.59,
+        ttSellingPrice: 1353.37,
+      },
+      isLoading: false,
+      isFetching: false,
+      onChange: jest.fn(),
+    });
+
+    const tree = renderer
+      .create(createWrapper(<EuroInfoView />, fakeApi))
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
