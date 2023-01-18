14f5d7117e200ab7cb0d6bf74b29571dea7aa992
geunhong Lim <seelime11@gmail.com>
2023-01-18 11:18:02 +0900

    feat: 불필요한 주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,6 @@ import InputView from "./view/InputView";
 export const App = () => {
   const [isReady, setReady] = useState(false);
 
-  // const getEurInfo = async () => {
-  //   const krweur = await fetch(
-  //     "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((array) => array[0]);
-
-  //   setEurInfo(krweur);
-  //   setReady(true);
-  // };
-
-  // const exchangeEurToKrw = (krw: any) => krw * eurInfo.basePrice;
-
   return (
     <div className="App">
       <ExchangeInfoView />
