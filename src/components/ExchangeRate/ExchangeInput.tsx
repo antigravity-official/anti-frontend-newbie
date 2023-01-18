80603cb0e64ece45db2ee4109b4fd254756fbea6
cys0926 <cys0926ok@gmail.com>
2023-01-18 11:24:03 +0900

    소수점 2개 입력 에러처리

--- src/components/ExchangeRate/ExchangeInput.tsx
+++ src/components/ExchangeRate/ExchangeInput.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useCallback, useRef, useState } from "react";
 import { ExchangeInfo } from "../../../typing";
-import { addComma, formattingWon, unComma } from "../../utils";
+import { addComma, unComma } from "../../utils";
 
 interface Props {
   exchangeInfo: ExchangeInfo;
@@ -11,65 +11,29 @@ const ExchangeInput = ({ exchangeInfo }: Props) => {
   const [inputError, setInputError] = useState("");
   const { basePrice } = exchangeInfo;
 
-  const exchangeEurToKrw = useCallback((money: string) => {
-    const pureMoney = Number(unComma(money));
+  const exchangeEurToKrw = useCallback((inputMoney: string) => {
+    const pureMoney = Number(unComma(inputMoney));
     const newBasePrice = Math.floor(basePrice);
     const exchangedMoney = Math.floor(pureMoney * newBasePrice);
     const formattingMoney = addComma(String(exchangedMoney));
     return formattingMoney;
   }, []);
 
-  // dependency 에 money 를 넣고 prevMoney.current 대신 money 를 사용해도 되지만
-  // 그렇게 하면 money 가 변경될 때마다 onMoneyChange 가 재생성되기 때문에 useCallback 을 사용하는 이유가 없다.
-  // const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-  //   const value = event.target.value;
-  //   const unCommaValue = unComma(value);
-  //   const formatRegex = /^\d{1,3}(,d{3})*$/;
-  //
-  //   const checkInputDot = (prev: string, curr: string) => {
-  //     return (
-  //       /^\d{1,3}(,d{3})*$/.test(prev) && /^\d{1,3}(,\d{3})*[.]$/.test(curr)
-  //     );
-  //   };
-  //
-  //   const checkDeleteDot = (prev: string, curr: string) => {
-  //     return (
-  //       /^\d{1,3}(,\d{3})*[.]\d{1}$/.test(prev) &&
-  //       /^\d{1,3}(,\d{3})*[.]$/.test(curr)
-  //     );
-  //   };
-  //
-  //   // 소수점을 입력했을때 실행
-  //   if (checkInputDot(prevMoney.current, value)) {
-  //     console.log("1");
-  //     prevMoney.current = addComma(unCommaValue) + ".";
-  //     return setMoney(addComma(unCommaValue) + ".");
-  //   }
-  //   // 소수점을 지웠을때 실행
-  //   if (checkDeleteDot(prevMoney.current, value)) {
-  //     console.log("2");
-  //     prevMoney.current = addComma(unCommaValue).replace(".", "");
-  //     return setMoney(addComma(unCommaValue).replace(".", ""));
-  //   }
-  //   // 일반적인 실행
-  //   if (/^\d{1,3}(,\d{3})*([.]\d{1,2})*$/.test(value)) {
-  //     console.log("3");
-  //     prevMoney.current = addComma(unCommaValue);
-  //     return setMoney(addComma(unCommaValue));
-  //   }
-  //   // 모두 지웠을때 실행
-  //   if (!value) {
-  //     prevMoney.current = "0";
-  //     return setMoney("0");
-  //   }
-  // }, []);
-
   // prevInteger 는 이전 숫자를 기억하고 사용자 커서 위치를 조작하는데 사용
   const prevInteger = useRef("");
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInputError("");
-    const [integer, decimal] = e.target.value.split(".");
-    const caret = e.target.selectionStart ?? e.target.value.length;
+    const { value } = e.target;
+    const [integer, decimal, notValid] = value.split(".");
+    const caret = e.target.selectionStart ?? value.length;
+    // "."을 2개 이상 입력했다면 early return
+    if (notValid !== undefined) {
+      window.requestAnimationFrame(() => {
+        e.target.selectionStart = caret - 1;
+        e.target.selectionEnd = caret - 1;
+      });
+      return setInputError("소수점은 한번만 입력할 수 있습니다.");
+    }
     // 만약 소수점을 3자리 이상 입력했다면 early return
     if (decimal?.length > 2) {
       window.requestAnimationFrame(() => {
@@ -89,17 +53,12 @@ const ExchangeInput = ({ exchangeInfo }: Props) => {
     const pureInteger = unComma(integer);
     const formattedInteger = addComma(pureInteger);
     const formattedDecimal = decimal?.slice(0, 2);
-
     const fullMoney =
       formattedDecimal === undefined
         ? formattedInteger
         : formattedInteger + "." + formattedDecimal;
-
-    setMoney(fullMoney);
-
+    // 현재 입력 값과 이전 입력 값을 비교해서 커서의 위치를 조작
     const diffLength = formattedInteger.length - prevInteger.current.length;
-    console.log("caret", caret);
-    console.log(diffLength);
     if (diffLength === 2) {
       // 콤마가 추가됐을 경우 커서 위치 유지
       window.requestAnimationFrame(() => {
@@ -121,6 +80,8 @@ const ExchangeInput = ({ exchangeInfo }: Props) => {
         e.target.selectionEnd = caret;
       });
     }
+
+    setMoney(fullMoney);
     prevInteger.current = formattedInteger;
   }, []);
 
