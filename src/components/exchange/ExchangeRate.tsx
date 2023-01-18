71e75c54657bf7be9ed9cf7e7330abe08a03a8b1
Yeunhun Bae <sbaru1997@naver.com>
2023-01-19 02:00:18 +0900

    feat: 환율 정보 view 금액 소수점, 콤마(,) 처리 구현
    금액은 세자리 마다 콤마(,) 표시
    유로화는 소수점 2자리까지 표시하고, 원화는 소수점 미표시

--- src/components/exchange/ExchangeRate.tsx
+++ src/components/exchange/ExchangeRate.tsx
@@ -1,4 +1,5 @@
 import { ExchangeRateInfo } from "../../model/ExchangeRateInfo";
+import { getFormattedNumber } from "../../utils/Util";
 
 interface ExchangeRateProps {
   country: { korName: string; enName: string };
@@ -6,20 +7,28 @@ interface ExchangeRateProps {
 }
 
 const ExchangeRate = ({ country, exchangeRateInfo }: ExchangeRateProps) => {
+  const exchangePrice = {
+    basePrice: getFormattedNumber(exchangeRateInfo.basePrice),
+    changePrice: getFormattedNumber(exchangeRateInfo.changePrice),
+    cashBuyingPrice: getFormattedNumber(exchangeRateInfo.cashBuyingPrice),
+    cashSellingPrice: getFormattedNumber(exchangeRateInfo.cashSellingPrice),
+    ttSellingPrice: getFormattedNumber(exchangeRateInfo.ttSellingPrice),
+    ttBuyingPrice: getFormattedNumber(exchangeRateInfo.ttBuyingPrice),
+  };
   return (
     <div className="exchange__container">
       <div className="exchange-title">환율기준 (1 {country.korName})</div>
       <div className="exchange-rate">
-        {exchangeRateInfo.basePrice}
+        {exchangePrice.basePrice}
         {exchangeRateInfo.isPriceUp() && "▲"}
         {exchangeRateInfo.isPriceDown() && "▼"}
-        {exchangeRateInfo.changePrice}원 ({exchangeRateInfo.getPriceRate()}%)
+        {exchangePrice.changePrice}원 ({exchangeRateInfo.getPriceRate()}%)
       </div>
       <div className="exchange-prices__container">
-        <div className="exchange-price">살때 : {exchangeRateInfo.cashBuyingPrice}</div>
-        <div className="exchange-price">팔때 : {exchangeRateInfo.cashSellingPrice}</div>
-        <div className="exchange-price">보낼때 : {exchangeRateInfo.ttSellingPrice}</div>
-        <div className="exchange-price">받을때 : {exchangeRateInfo.ttBuyingPrice}</div>
+        <div className="exchange-price">살때 : {exchangePrice.cashBuyingPrice}원</div>
+        <div className="exchange-price">팔때 : {exchangePrice.cashSellingPrice}원</div>
+        <div className="exchange-price">보낼때 : {exchangePrice.ttSellingPrice}원</div>
+        <div className="exchange-price">받을때 : {exchangePrice.ttBuyingPrice}원</div>
       </div>
     </div>
   );
