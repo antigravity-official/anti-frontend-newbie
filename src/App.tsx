3d3c3a233ab9f3be98432700a308489030ebc417
jwzzang12 <moonbb12@gmail.com>
2023-01-18 23:54:23 +0900

    Add loading/error UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from "react";
 
 export const App = () => {
   const [isReady, setReady] = useState(false);
+  const [error, setError] = useState<boolean>(false);
+
   const [eurInfo, setEurInfo] = useState<any>({});
   const [result, setResult] = useState<number>(0);
   const [stopInput, setStopInput] = useState<boolean>(false);
 
   const getEurInfo = async () => {
-    const krweur = await fetch("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR")
-      .then((response) => response.json())
-      .then((array) => array[0]);
-
-    setEurInfo(krweur);
     setReady(true);
+    try {
+      const krweur = await fetch("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR")
+        .then((response) => response.json())
+        .then((array) => array[0]);
+      setEurInfo(krweur);
+    } catch {
+      setError(true);
+    }
+    setReady(false);
   };
 
   const exchangeEurToKrw = (e: any) => {
@@ -36,10 +42,8 @@ export const App = () => {
     return () => {};
   }, []);
 
-  if (!isReady) return null;
-  return (
-    <div className="App">
-      <div>환율기준 (1 유로)</div>
+  let content = (
+    <>
       <div>
         {eurInfo.basePrice}
         {eurInfo.basePrice - eurInfo.openingPrice > 0 && "▲"}
@@ -52,6 +56,21 @@ export const App = () => {
         <div>보낼때 : {eurInfo.ttSellingPrice}</div>
         <div>받을때 : {eurInfo.ttBuyingPrice}</div>
       </div>
+    </>
+  );
+
+  if (error) {
+    content = <p>알 수 없는 오류가 발생하였습니다. 잠시 후 다시 접속해주세요.</p>;
+  }
+
+  if (isReady) {
+    content = <p>로딩 중 입니다...</p>;
+  }
+
+  return (
+    <div className="App">
+      <div>환율기준 (1 유로)</div>
+      {content}
       <hr />
       <input onChange={exchangeEurToKrw} disabled={stopInput} /> 유로 ▶︎ <input disabled value={eurToKrw} /> 원
       {stopInput && <div>유로화는 소수점 2자리까지만 입력이 가능합니다.</div>}
