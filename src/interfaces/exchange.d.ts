7cc6a8bb8b54407b170431df6d8f2a8694cc5263
GeonWooPaeng <geonwoopaeng@gmail.com>
2023-01-19 01:09:12 +0900

    feat: setting Exchange data types
    KrwEurInfosTypes
    : Exchange between krw and eur information

---
+++ src/interfaces/exchange.d.ts
@@ -0,0 +1,75 @@
+export interface KrwEurInfosTypes {
+  code: string;
+  currencyCode: string;
+  currencyName: string;
+  country: string;
+  name: string;
+  date: string;
+  time: string;
+  recurrenceCount: number;
+  basePrice: number;
+  openingPrice: number;
+  highPrice: number;
+  lowPrice: number;
+  change: string;
+  changePrice: number;
+  cashBuyingPrice: number;
+  cashSellingPrice: number;
+  ttBuyingPrice: number;
+  ttSellingPrice: number;
+  tcBuyingPrice: null;
+  fcSellingPrice: null;
+  exchangeCommission: number;
+  usDollarRate: number;
+  high52wPrice: number;
+  high52wDate: string;
+  low52wPrice: number;
+  low52wDate: string;
+  currencyUnit: number;
+  provider: string;
+  timestamp: number;
+  id: number;
+  createdAt: string;
+  modifiedAt: string;
+  changeRate: number;
+  signedChangePrice: number;
+  signedChangeRate: number;
+}
+
+// {
+//   "code": "FRX.KRWEUR",
+//   "currencyCode": "EUR",
+//   "currencyName": "유로",
+//   "country": "유로",
+//   "name": "유로 (KRW/EUR)",
+//   "date": "2023-01-18",
+//   "time": "16:40:19",
+//   "recurrenceCount": 870,
+//   "basePrice": 1337.03,
+//   "openingPrice": 1336.47,
+//   "highPrice": 1342.78,
+//   "lowPrice": 1333.34,
+//   "change": "FALL",
+//   "changePrice": 1.67,
+//   "cashBuyingPrice": 1363.63,
+//   "cashSellingPrice": 1310.43,
+//   "ttBuyingPrice": 1323.66,
+//   "ttSellingPrice": 1350.40,
+//   "tcBuyingPrice": null,
+//   "fcSellingPrice": null,
+//   "exchangeCommission": 3.9450,
+//   "usDollarRate": 1.0813,
+//   "high52wPrice": 1440.20,
+//   "high52wDate": "2022-10-27",
+//   "low52wPrice": 1306.52,
+//   "low52wDate": "2022-07-12",
+//   "currencyUnit": 1,
+//   "provider": "하나은행",
+//   "timestamp": 1674027625020,
+//   "id": 25,
+//   "createdAt": "2016-10-21T06:13:30.000+0000",
+//   "modifiedAt": "2023-01-18T07:40:25.000+0000",
+//   "changeRate": 0.0012474789,
+//   "signedChangePrice": -1.67,
+//   "signedChangeRate": -0.0012474789
+// }
